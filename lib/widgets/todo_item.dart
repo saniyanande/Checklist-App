4182be8892a5e2a8e633f8d4ecfd8e6dd78879c5
saniya <saniyanande2@gmail.com>
2023-06-22 22:47:21 +0530

    commit

---
+++ lib/widgets/todo_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import '../constants/colors.dart';
+
+class ToDoItem extends StatelessWidget {
+  const ToDoItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 20),
+      child: ListTile(
+        onTap: () {
+          print('clicked clicked');
+        },
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        contentPadding: EdgeInsets.symmetric(horizontal: 20,vertical: 5),
+        tileColor: Colors.white,
+        leading: const Icon(
+          Icons.check_box,
+          color: tdBLue,
+        ),
+        title: const Text(
+          'Strike out ASAP',
+          style: TextStyle(
+            fontSize: 16,
+            color: tdBlack,
+            decoration: TextDecoration.lineThrough,
+          ),
+        ),
+        trailing: Container(
+          padding: EdgeInsets.all(0),
+          margin: EdgeInsets.symmetric(vertical: 12),
+          height: 50,
+          width: 30,
+          decoration: BoxDecoration(
+            color: tdRed,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: IconButton(
+            color: Colors.white,
+            iconSize: 18,
+            icon: const Icon(Icons.delete),
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
+  }
+}
